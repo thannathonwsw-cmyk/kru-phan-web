e5163475ff8558ac0a9f873ec4aedb657dda8c29
thannathonwsw-cmyk <thannathon.wsw@gmail.com>
2026-09-16 09:06:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+const APP_CONFIG = {
+  clientId: 'วาง_APPLICATION_CLIENT_ID_ที่นี่',
+  tenant: 'common',
+  filePath: 'ครุภัณฑ์/assets.xlsx',
+  imagePath: 'ครุภัณฑ์/images',
+  table: 'Assets',
+  statuses: ['รอตรวจรับ','ใช้งานปกติ','ถูกยืม','ชำรุด/รอซ่อม','รอจำหน่าย','จำหน่ายแล้ว','สูญหาย'],
+  categories: ['คอมพิวเตอร์','โสตทัศนศึกษา','กล้องถ่ายภาพ','เครื่องเสียง','อุปกรณ์สำนักงาน','อื่น ๆ']
+};
