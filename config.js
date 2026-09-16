cb6e7ca4eafa12a152e3544279a3ef3eec21ec55
thannathonwsw-cmyk <thannathon.wsw@gmail.com>
2026-09-16 09:15:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
-  clientId: 'วาง_APPLICATION_CLIENT_ID_ที่นี่',
-  tenant: 'common',
+  clientId: '8d6c06f5-f5c7-4c11-bc25-4a4e881cd9ef',
+  tenant: '8e634e67-9d66-46d2-a529-e1b708c5d8bc',
   filePath: 'ครุภัณฑ์/assets.xlsx',
   imagePath: 'ครุภัณฑ์/images',
   table: 'Assets',
