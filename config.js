b56166a480c5e7132f7379609544c32e4bcc5189
thannathonwsw-cmyk <thannathon.wsw@gmail.com>
2026-09-17 09:39:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,6 @@ const APP_CONFIG = {
   supabaseUrl: 'https://qyinewbveoocpltyffou.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5aW5ld2J2ZW9vY3BsdHlmZm91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1OTQyMDIsImV4cCI6MjEwNTE3MDIwMn0._s9HBk-dsndZlRy3j0cRBQ_nm1QdX7XGpj8XoWiYDsU',
   bucket: 'kru-images',
-  filePath: 'ครุภัณฑ์/assets.xlsx',
-  imagePath: 'ครุภัณฑ์/images',
-  table: 'Assets',
   statuses: ['รอตรวจรับ','ใช้งานปกติ','ถูกยืม','ชำรุด/รอซ่อม','รอจำหน่าย','จำหน่ายแล้ว','สูญหาย'],
   categories: ['คอมพิวเตอร์','โสตทัศนศึกษา','กล้องถ่ายภาพ','เครื่องเสียง','อุปกรณ์สำนักงาน','อื่น ๆ']
 };
